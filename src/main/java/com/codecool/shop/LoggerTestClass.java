e4714d1380335fd189acda6eb272a60de92b7b3c
unknown <peter.codecool@gmail.com>
2017-05-27 17:35:04 +0200

    Add JavaDoc for Test Logger

--- src/main/java/com/codecool/shop/LoggerTestClass.java
+++ src/main/java/com/codecool/shop/LoggerTestClass.java
@@ -7,13 +7,31 @@ import java.util.Calendar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * <h1>Logger Test Class With JavaDoc class</h1>
+ * <p>Logger tester class for the exercise</p>
+ */
+
 public class LoggerTestClass {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     public static void main( String[] args )
     {
+
+        /**
+         * User parameters declarating
+         *
+         * @param user temperature
+         */
         String user = "john";
         String application = "gateway";
+
+        /**
+         * Log a message at specified format and argument.<p/>
+         *
+         * @param user the format string
+         * @param Calendar    the argument
+         */
         LOGGER.info("Bad experience for user " + user + " at time " + Calendar.getInstance().getTime());
         LOGGER.info("Bad experience for user {}", user);
         LOGGER.info("Bad experience for user {}");
